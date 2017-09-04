19f716af761c0c70bb6ec60b102ddea6a5fc0388
Graham Ollis <plicease@cpan.org>
2017-09-04 19:08:52 -0400

    doco

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 Override on a perl-alien basis
 
+# SYNOPSIS
+
+in your `~/.alienbuild/rc.pl`:
+
+    preload 'Probe::Override';
+    
+    sub override {
+      my($dist) = @_;
+      return 'system'  if $dist eq 'Alien-gmake';
+      return 'share'   if $dist eq 'Alien-FFI';
+      return 'default' if $dist eq 'Alien-libuv';  # lets the alienfile choose
+      return ''; # fall back on $ENV{ALIEN_INSTALL_TYPE}
+    };
+
+# DESCRIPTION
+
+This [alienfile](https://metacpan.org/pod/alienfile) plugin allows you to override the install type (either
+`share`, `system` or `default`.
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/Alien/Build/Plugin/Probe/Override.pm
+++ lib/Alien/Build/Plugin/Probe/Override.pm
@@ -9,6 +9,27 @@ use Path::Tiny qw( path );
 # ABSTRACT: Override on a perl-alien basis
 # VERSION
 
+=head1 SYNOPSIS
+
+in your C<~/.alienbuild/rc.pl>:
+
+ preload 'Probe::Override';
+ 
+ sub override {
+   my($dist) = @_;
+   return 'system'  if $dist eq 'Alien-gmake';
+   return 'share'   if $dist eq 'Alien-FFI';
+   return 'default' if $dist eq 'Alien-libuv';  # lets the alienfile choose
+   return ''; # fall back on $ENV{ALIEN_INSTALL_TYPE}
+ };
+
+=head1 DESCRIPTION
+
+This L<alienfile> plugin allows you to override the install type (either
+C<share>, C<system> or C<default>.
+
+=cut
+
 sub init
 {
   my($self, $meta) = @_;
