64e864aab1e1336887cbd0552960a86f61955437
Graham Ollis <plicease@cpan.org>
2017-09-04 12:22:24 -0400

    Initial commit

---
+++ lib/Alien/Build/Plugin/Probe/Override.pm
@@ -0,0 +1,10 @@
+package Alien::Build::Plugin::Probe::Override;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Override on a perl-alien basis
+# VERSION
+
+1;
---
+++ t/alien_build_plugin_probe_override.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Alien::Build::Plugin::Probe::Override;
+
+ok 1, 'todo';
+
+done_testing
