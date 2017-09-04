fe5502079aa866ff02d108da7a6456f395332069
Graham Ollis <plicease@cpan.org>
2017-09-04 17:00:32 -0400

    initial implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Alien::Build::Plugin::Probe::Override [![Build Status](https://secure.travis-ci.org/plicease/Alien-Build-Plugin-Probe-Override.png)](http://travis-ci.org/plicease/Alien-Build-Plugin-Probe-Override)
+
+Override on a perl-alien basis
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Alien/Build/Plugin/Probe/Override.pm
+++ lib/Alien/Build/Plugin/Probe/Override.pm
@@ -3,8 +3,37 @@ package Alien::Build::Plugin::Probe::Override;
 use strict;
 use warnings;
 use 5.008001;
+use Alien::Build::Plugin;
+use Path::Tiny qw( path );
 
 # ABSTRACT: Override on a perl-alien basis
 # VERSION
+
+sub init
+{
+  my($self, $meta) = @_;
+  
+  $meta->register_hook(
+    override => sub {
+      my($build) = @_;
+      
+      if(Alien::Build::rc->can('override'))
+      {
+        my $class = path($build->install_prop->{stage})->basename;
+        if($class =~ /^Alien-/)
+        {
+          my $override = Alien::Build::rc::override($class);
+          if($override)
+          {
+            return $override if $override =~ /^(system|share|default)$/;
+            $build->log("override for $class => $override is not valid");
+          }
+        }
+      }
+      
+      $ENV{ALIEN_INSTALL_TYPE} || '';
+    },
+  );
+}
 
 1;
